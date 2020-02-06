3d949b18118d6c656bdc85a7917b5429f333b144
Matthew Robinson <matt_robinson1@yahoo.com>
2020-02-06 13:50:20 +0000

    Automatically backed up by Learn

--- src/components/todos/TodosContainer.js
+++ src/components/todos/TodosContainer.js
@@ -1,10 +1,13 @@
-import React, { Component} from 'react'; 
-import { connect } from 'react-redux'; 
-
+import React, { Component } from 'react';
+import { connect } from 'react-redux'
+ 
 class TodosContainer extends Component {
+ 
   render() {
     return(
-      <div></div> 
+      <div></div>
     );
   }
 };
+ 
+export default connect()(TodosContainer);
